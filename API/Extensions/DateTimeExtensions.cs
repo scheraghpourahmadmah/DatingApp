1e6c836d6c724e283cbdd21733ddc13348f5ac29
scheraghpourahmadmah <scheraghpourahmadmah@myseneca.ca>
2023-01-16 16:42:23 -0500

    Extending the User Entity

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalcuateAge(this DateTime dob)
+        {
+            var today = DateTime.UtcNow; 
+
+            var age = today.Year - dob.Year;
+
+            if (dob > today.AddYears(-age)) --age;
+
+            return age; 
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,11 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,5 +30,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
